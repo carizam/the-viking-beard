b991069b22055caca533305468625e01099722a3
carizam <ciarizam@hotmail.com>
2024-02-12 21:38:15 -0500

    Mongo added

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const productsRoutes = require("./src/routes/products");
 const cartsRoutes = require("./src/routes/carts");
 const app = express();
 
 app.use(express.json()); // Middleware para parsear JSON
 
-// Ruta productos
+// Configuración de Mongoose para conectarse a MongoDB Atlas
+const mongoDBAtlasUri =
+  "mongodb+srv://ciarizam:<password>@vikingbearddb.12u8the.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(mongoDBAtlasUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.error(err));
+
+// Rutas para productos
 app.use("/api/products", productsRoutes);
 
-// Ruta  carritos
+// Rutas para carritos
 app.use("/api/carts", cartsRoutes);
 
-const PORT = 8080;
+// Iniciar servidor
+const PORT = process.env.PORT || 8080; // Usar variable de entorno para el puerto o 8080 como predeterminado
 app.listen(PORT, () => console.log(`Servidor en el puerto ${PORT}`));
