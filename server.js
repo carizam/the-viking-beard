43573a6ea5e250fc43f3ae14f5eafcd1aa3676ae
carizam <ciarizam@hotmail.com>
2024-02-20 16:47:23 -0500

    Fixed Mongo connection

--- server.js
+++ server.js
@@ -7,15 +7,25 @@ const app = express();
 app.use(express.json()); // Middleware para parsear JSON
 
 // Configuración de Mongoose para conectarse a MongoDB Atlas
-const mongoDBAtlasUri =
-  "mongodb+srv://ciarizam:<password>@vikingbearddb.12u8the.mongodb.net/?retryWrites=true&w=majority";
+
+const password = encodeURIComponent("HYPO987");
+const mongoDBAtlasUri = `mongodb+srv://ciarizam:${password}@vikingbearddb.12u8the.mongodb.net/?retryWrites=true&w=majority`;
+
 mongoose
-  .connect(mongoDBAtlasUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(mongoDBAtlasUri)
   .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.error(err));
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Opcional: Manejo de errores de conexión inicial y reconexión
+mongoose.connection.on("error", (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+  process.exit(-1); // Opcional: termina el proceso en caso de un error de conexión
+});
+
+// Opcional: Evento de reconexión (útil para el seguimiento)
+mongoose.connection.on("reconnected", () => {
+  console.log("MongoDB reconnected!");
+});
 
 // Rutas para productos
 app.use("/api/products", productsRoutes);
@@ -24,5 +34,5 @@ app.use("/api/products", productsRoutes);
 app.use("/api/carts", cartsRoutes);
 
 // Iniciar servidor
-const PORT = process.env.PORT || 8080; // Usar variable de entorno para el puerto o 8080 como predeterminado
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Servidor en el puerto ${PORT}`));
